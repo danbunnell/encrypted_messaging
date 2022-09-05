1e36e460f6e80deb5547106f19f7673649c1170d
Dan Bunnell <bunnell.dan@gmail.com>
2022-09-05 19:53:00 -0400

    Remove unused fields

--- src/messaging.py
+++ src/messaging.py
@@ -6,8 +6,6 @@ from crypto.types import KeyAlgorithm
 
 class MessagingService:
     """A service which provides messaging functionality"""
-    RSA_EXPONENT: int = 65537
-    RSA_KEY_SIZE: int = 2048
 
     def gen_key(self, algorithm: KeyAlgorithm) -> RSAPrivateKey:
         """Generates a key of the specified type"""
