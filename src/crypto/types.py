3b342676247557d92ad595477ba5317d25e6342d
Dan Bunnell <bunnell.dan@gmail.com>
2022-09-06 21:01:26 -0400

    Fix misleading comments

--- src/crypto/types.py
+++ src/crypto/types.py
@@ -11,11 +11,11 @@ class KeyAlgorithm(Enum):
     only be used for signing."""
 
     RSA = 2
-    """The River-Shamir-Adleman algorithm, relies on integer factorization, most widely\
-    used, requires larger key sizes (2048+ bits) to be secure, slower generation.      \
-    Can be used for signing and encryption. """
+    """The Rivest-Shamir-Adleman algorithm, relies on integer factorization, most      \
+    widely used, but requires larger key sizes (2048+ bits) to be secure, and provides \
+    slower key generation. Can be used for signing and encryption."""
 
     ECDSA = 3
-    """An elliptical curve Digitial Signature Algorithm which is based on the NIST    \
-    p-256 curve. Some question the security of the curve, but this is the most        \
-    commonly supported ECC algorithm. Can only be used for signing."""
+    """An elliptical curve Digitial Signature Algorithm which may be vulnerable to     \
+    insufficient randomization in the RNG, but is the most commonly supported ECC      \
+    algorithm used for TLS. Can only be used for signing."""
